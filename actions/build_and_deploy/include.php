ac8e39de52c6636f86f4333d501e04b775eb6dc4
Edwin Rodriguez <edwrodrig@gmail.com>
2024-01-26 21:14:21 -0300

    add include to build_and_deploy

---
+++ actions/build_and_deploy/include.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+require_once(__DIR__ . '/../include.php');
+
+const LOCAL_CONFIG_DIR = __DIR__ . '/config';
+
+const LOCAL_CONFIG_DEPLOY_GITHUB = __DIR__ . '/deploy_github.ini';
+
+const LOCAL_CONFIG_LIST = [
+    LOCAL_CONFIG_DEPLOY_GITHUB
+];
