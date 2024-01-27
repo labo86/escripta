f508054e00cc3ef0320cb62310bb2bc353c3ba5c
Edwin Rodriguez <edwrodrig@gmail.com>
2024-01-26 23:07:52 -0300

    fix bar local config deploy github param

--- actions/build_and_deploy/include.php
+++ actions/build_and_deploy/include.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . '/../include.php');
 
 const LOCAL_CONFIG_DIR = __DIR__ . '/config';
 
-const LOCAL_CONFIG_DEPLOY_GITHUB = __DIR__ . '/deploy_github.ini';
+const LOCAL_CONFIG_DEPLOY_GITHUB = 'deploy_github';
 
 const LOCAL_CONFIG_LIST = [
     LOCAL_CONFIG_DEPLOY_GITHUB
