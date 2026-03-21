83c0e4c61cf7b4229b4f85dd2e528a41eaed82d7
Edwin Rodríguez <edwrodrig@gmail.com>
2026-03-21 08:34:21 -0300

    feat: clena changes

--- .escripta/manage_escripta/update.sh
+++
@@ -1,8 +0,0 @@
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-PHAR_URL="https://github.com/labo86/escripta/releases/latest/download/escripta.phar"
-TARGET_PHAR="$SCRIPT_DIR/../escripta.phar"
-
-cd "$SCRIPT_DIR" || exit
-
-rm -f "$TARGET_PHAR"
-curl -fsSL "$PHAR_URL" -o "$TARGET_PHAR"
